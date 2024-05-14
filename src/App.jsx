117e9f41da514580edf2f5d49d6ae409de51394e
Arianit Gashi <gashiarianit2003@gmail.com>
2024-05-14 23:38:28 +0200

    Implement the needed sections about the dashboard including home search support and info
    Implement the necessary sections for a dashboard, including Home, Search, Support, and Info. The dashboard should provide a user-friendly interface with clear navigation and easy access to essential features

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Navbar from './Components/Navbar/Navbar'
-
+import Home from './Components/Home/Home'
+import Search from './Components/Search/Search'
+import Support from './Components/Support/Support'
+import Info from './Components/Info/Info'
 
 import './main.scss'
 
@@ -8,6 +11,10 @@ function App() {
   return (
    <div>
     <Navbar />
+    <Home />
+    <Search />
+    <Support />
+    <Info />
    </div>
   )
 }
